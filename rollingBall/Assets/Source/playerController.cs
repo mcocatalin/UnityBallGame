70fd86532253f6292e90aeec2c12e1b9e8b61d9a
Catalin Milea <milea.cata@yahoo.com>
2017-07-20 00:06:41 +0300

    first commit
    step 3 ball game unity 3D using c# script

---
+++ rollingBall/Assets/Source/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public Rigidbody ballRB;
+    public float speed = 10f;
+
+
+    void Update() // movement per frame
+    {
+
+    }
+
+    void Start()
+    {
+        ballRB = new Rigidbody();
+        ballRB = GetComponent<Rigidbody>();
+        speed = 100f;
+    }
+
+    void FixedUpdate() // physics init
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 heading = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        ballRB.AddForce(heading * speed);
+    }
+
+}
